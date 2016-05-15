fbb435e1402d5204066ad5635821e69d731491b0
amirhmz <amdark77@gmail.com>
2016-05-15 20:21:35 +0430

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -1,7 +1,7 @@
 do
 -- Will leave the group if be added
 local function run(msg, matches)
-local bot_id = our_id 
+local bot_id = 172490081
 local receiver = get_receiver(msg)
     if matches[1] == 'leave' and is_admin1(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
