a989d3ae13b0768bf7e145bbc557e386ba0c79ee
amirhmz <amdark77@gmail.com>
2016-05-16 13:04:38 +0430

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,34 +1,27 @@
- -- FeedBack Plugin For Xamarin Source v1.2
- --https://github.com/amirhmz
-
 do
-
  function run(msg, matches)
- local ch = 'chat#id'..msg.to.id
- local fuse = '📌 #فیدبک جدید\n\n👤 نام کاربر : ' .. msg.from.print_name .. '\n\n👤 نام کاربری : @' .. msg.from.username ..'\n\n👤 کد کاربر : ' .. msg.from.id ..'\n\n👤 کد گروه : '..msg.to.id.. '\n\n📝 متن پیام :\n\n' .. matches[1]
+ 
+ local fuse = '📌 #فیدبک جدید\n\n🆔 کد کاربری : ' .. msg.from.id .. '\n\n👤 نام کاربر : ' .. msg.from.print_name ..'\n\n🔢 نام کاربری : @' .. msg.from.username .. '\n\n📬 متن پیام :\n\n' .. matches[1] 
  local fuses = '!printf user#id' .. msg.from.id
-
-
+ 
+ 
    local text = matches[1]
-   local chat = "chat#id"..83535263
-
+   local chat = "user#id"..119626024
+   --like : local chat = "channel#id"..12345678
+   
   local sends = send_msg(chat, fuse, ok_cb, false)
-  return '✅ پیام شما ارسال شد'
-
+  return '📝 پیام شما دریافت شد'
+ 
  end
  end
  return {
-
-  description = "Feedback",
-
-  usage = "feedback: Send A Message To Admins.",
-  patterns = {
-  "^[!/][Ff]eedback (.*)$",
-  "^[Ff]eedback (.*)$"
   
+  description = "Feedback",
+ 
+  usage = "!feedback message",
+  patterns = {
+  "^[!#/][Ff]eedback (.*)$"
+ 
   },
   run = run
  }
- 
- --https://github.com/amirhmz
- --Telegram.me/AmirDark
