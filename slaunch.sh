6641b00bac4103e82be18b2f05caf41518a31610
amirhmz <amdark77@gmail.com>
2016-05-20 12:53:06 +0430

    Added Auto Launch :)

--- slaunch.sh
+++ slaunch.sh
@@ -111,5 +111,5 @@ else
     exit 1
   fi
 #By @PokerFace_Dev
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/seedbot.lua -l 1 -E $@
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/xamarin.lua -l 1 -E $@
 fi
