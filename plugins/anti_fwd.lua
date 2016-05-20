4c9f6c1c6fb3557facf24e139e616578b223bd01
amirhmz <amdark77@gmail.com>
2016-05-20 17:59:04 +0430

    Update anti_fwd.lua

--- plugins/anti_fwd.lua
+++ plugins/anti_fwd.lua
@@ -23,11 +23,11 @@ local function run(msg, matches)
             
                     local hash = 'mate:'..msg.to.id
                     redis:set(hash, true)
-                    return "قفل فروارد فعال شد"
+                    return "قفل فوروارد فعال شد"
   elseif is_momod(msg) and matches[1] == 'unlock' then
                     local hash = 'mate:'..msg.to.id
                     redis:del(hash)
-                    return "قفل فروارد غیر فعال شد"
+                    return "قفل فوروارد غیر فعال شد"
 end
 
 end
