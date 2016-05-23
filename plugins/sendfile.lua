f272d4de49984a3eba017208cf23543662fff55d
amirhmz <amdark77@gmail.com>
2016-05-23 13:56:39 +0430

    Update sendfile.lua

--- plugins/sendfile.lua
+++ plugins/sendfile.lua
@@ -1,7 +1,7 @@
 do
 local solid = 119626024
 local function run(msg, matches)
-  if matches[1] == "dl" and matches[2] and matches[3] then
+  if matches[1] == "sendfile" and matches[2] and matches[3] then
     if msg.from.id == tonumber(solid) then
 	    local file = "./"..matches[2].."/"..matches[3]..""
       local receiver = get_receiver(msg)
